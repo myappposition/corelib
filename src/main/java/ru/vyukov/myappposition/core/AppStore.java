33c9ddea749c2ef2201c24e4eaef94f27a284aaf
Oleg Vyukov <gelo@vyukov.ru>
2018-06-30 18:45:42 +0300

    Initial commit

---
+++ src/main/java/ru/vyukov/myappposition/core/AppStore.java
@@ -0,0 +1,9 @@
+package ru.vyukov.myappposition.core;
+
+/**
+ * @author Oleg Vyukov
+ */
+public enum AppStore {
+
+    apple, google, microsoft;
+}
---
+++ src/main/java/ru/vyukov/myappposition/core/MobilePlatform.java
@@ -0,0 +1,9 @@
+package ru.vyukov.myappposition.core;
+
+/**
+ * @author Oleg Vyukov
+ */
+public enum MobilePlatform {
+
+    android, ios, windows;
+}
